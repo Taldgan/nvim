900bdf4eba8da5cf0890fa70f22817b99122e5e4
Taldgan <bananaphone993@live.com>
2023-04-17 01:08:37 -0500

    mason, telescope, bufferline

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,21 @@
+local M = {
+  "nvim-telescope/telescope.nvim",
+  event = "Bufenter",
+  cmd = { "Telescope" },
+  dependencies = {
+    {
+      "ahmedkhalf/project.nvim",
+    },
+  },
+}
+
+M.opts = {
+  defaults = {
+    prompt_prefix = " ",
+    selection_caret = " ",
+    path_display = { "smart" },
+    file_ignore_patterns = { ".git/", "node_modules" },
+  },
+}
+
+return M
--- lua/plugkeybinds.lua
+++ lua/plugkeybinds.lua
@@ -11,3 +11,7 @@ keymap("", "<Space>", "<Nop>", opts)
 
 -- Nvim-Tree
 keymap("n", "<leader>e", ":NvimTreeToggle<CR>")
+
+-- Telescope
+keymap("n", "<leader>ff", ":Telescope find_files<CR>")
+keymap("n", "<leader>fF", ":Telescope live_grep<CR>")
